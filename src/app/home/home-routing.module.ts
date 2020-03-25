77e00f47ab8940bad67ce66771378745ea542175
ritesh suryavanshi <cubexo.ritesh@gmail.com>
2020-03-25 07:09:24 +0530

    create page ourservices, about us, Events, Or best deal

--- src/app/home/home-routing.module.ts
+++ src/app/home/home-routing.module.ts
@@ -1,13 +1,33 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
 import {HomeComponent} from './home.component';
 
+import {DashboardComponent} from './dashboard/dashboard.component';
+import {SpecialofferComponent} from './specialoffer/specialoffer.component';
+import {EventsComponent} from './events/events.component';
+import {AboutusComponent} from './aboutus/aboutus.component';
+import {OurservicesComponent} from './ourservices/ourservices.component';
+
 const routes: Routes = [
-  { path: 'home', component: HomeComponent },
+  {
+    path: 'home', component: HomeComponent,
+        children: [
+            { path: 'dashboard', component: DashboardComponent  },
+            { path: '', redirectTo: '/home/dashboard', pathMatch: 'full'},
+
+            { path: 'spcl', component: SpecialofferComponent  },
+            { path: 'events', component: EventsComponent  },
+            { path: 'aboutus', component: AboutusComponent  },
+            { path: 'services', component: OurservicesComponent  },
+
+    ]
+
+  },
 ];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
 })
-export class HomeRoutingModule { }
+export class HomeRoutingModule {
+}
--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -3,11 +3,21 @@ import { CommonModule } from '@angular/common';
 import { HomeRoutingModule } from './home-routing.module';
 
 import { HomeComponent } from './home.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { SpecialofferComponent } from './specialoffer/specialoffer.component';
+import { EventsComponent } from './events/events.component';
+import { AboutusComponent } from './aboutus/aboutus.component';
+import { OurservicesComponent } from './ourservices/ourservices.component';
 
 
 @NgModule({
   declarations: [
-    HomeComponent
+    HomeComponent,
+    DashboardComponent,
+    SpecialofferComponent,
+    EventsComponent,
+    AboutusComponent,
+    OurservicesComponent,
   ],
   imports: [
     CommonModule,
