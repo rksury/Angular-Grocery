fbeb63b5b889e9512a2f93e8e2ce6e60fbc925f3
devloper <rksury@gmail.com>
2020-04-14 01:43:57 +0530

    update order history

--- src/app/home/orderhistory/orderhistory.component.ts
+++ src/app/home/orderhistory/orderhistory.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {ProductsService} from '../products/products.service';
+import {CartService} from '../cart/cart.service';
 
 @Component({
   selector: 'app-orderhistory',
@@ -6,10 +8,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./orderhistory.component.css']
 })
 export class OrderhistoryComponent implements OnInit {
+   products = {};
+    showProducts = true;
+    special;
+    cart;
+    order;
 
-  constructor() { }
+  constructor( private productService: ProductsService,
+               private cartService: CartService) { }
 
   ngOnInit(): void {
+       this.products_data();
   }
-
+products_data() {
+        this.productService.get_all_products().subscribe(data => {
+            this.products = data;
+        });
+    }
 }
